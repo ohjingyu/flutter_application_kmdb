3714a8d3cf0b9fd75afd69390bb139d71e2b8872
ohjingyu <kyeong0644@gmail.com>
2023-06-07 18:38:20 +0900

    db

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:readmore/readmore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MovieDetail extends StatelessWidget {
   Map<String, dynamic> movie;
+
   MovieDetail({super.key, required this.movie});
 
   @override
@@ -11,7 +14,24 @@ class MovieDetail extends StatelessWidget {
         .toString()
         .replaceAll(' !HS ', '')
         .replaceAll(' !HE ', '');
-    var posters = movie['posters'].split('|')[0];
+    dynamic titleImage = movie['posters'].toString().isEmpty
+        ? Image.asset('assets/images/no_image.png')
+        : Image.network(movie['posters'].toString().split('|')[0]);
+    //var stills = movie['stlls'].toString().split('|');
+    List<Widget> stills = [];
+    if (movie['stlls'].toString().isEmpty) {
+      stills.add(Image.asset(
+        'assets/images/no_image.png',
+        fit: BoxFit.fitHeight,
+      ));
+    } else {
+      for (var k in movie['stlls'].toString().split('|')) {
+        stills.add(Image.network(
+          k,
+          fit: BoxFit.fitHeight,
+        ));
+      }
+    }
     return Scaffold(
       appBar: AppBar(title: Text('영화상세 정보($movieTitle)')),
       body: SingleChildScrollView(
@@ -21,7 +41,7 @@ class MovieDetail extends StatelessWidget {
             Row(
               children: [
                 Text(movie['keywords']),
-                Hero(tag: movie['movieSeq'], child: Image.network(posters))
+                Hero(tag: movie['movieSeq'], child: titleImage)
               ],
             ),
             ReadMoreText(
@@ -37,7 +57,17 @@ class MovieDetail extends StatelessWidget {
                   fontSize: 14,
                   fontWeight: FontWeight.bold,
                   color: Colors.blue),
-            )
+            ),
+            ImageSlideshow(
+                autoPlayInterval: 3000,
+                width: double.infinity,
+                height: 200,
+                children: stills),
+            ElevatedButton(
+                onPressed: () {
+                  launchUrl(Uri.parse(movie['kmdbUrl'].toString()));
+                },
+                child: Text('사이트 연결'))
           ],
         ),
       ),
